c828a9a415080782e24644b62a9a18411b808382
Ezike,Christopher <57494924+YOSMITECHRIS@users.noreply.github.com>
2020-06-06 00:33:57 +0100

    Add files via upload

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserCreateForm(UserCreationForm):
+
+        class Meta:
+          fields = ('username','email','password','password2')
+          model = get_user_model()
+
+        def _init_(self,*args,**kwargs):
+          super()._init_(*args,**kwargs)
+          self.fields['username'].label = 'Display Name'
+          self.fields['email'].label = "Email Address"   
